180dc46674a1ea94489469bc78e997bd1670f984
Denis <denis.pilipenko.90@gmail.com>
2022-10-06 19:58:56 +0200

    Linting

--- geomagnetic_storms/geo_storms_check.py
+++ geomagnetic_storms/geo_storms_check.py
@@ -1,16 +1,3 @@
-# import requests
-# import json
-
-# API_KEY = "DEMO_KEY"
-
-# def main():
-#     r = requests.get(f"https://api.nasa.gov/DONKI/GST?startDate=yyyy-MM-dd&endDate=yyyy-MM-dd&api_key={API_KEY}")
-#     response = json.loads(r.content)[0]
-#     print(response["linkedEvents"])
-
-# if __name__ == '__main__':
-#     main()
-
 import requests
 from datetime import datetime, timedelta
 from constants import API_KEY, NASA_API_ENDPOINT
@@ -20,7 +7,9 @@ time_start = time_now - timedelta(days=30)
 
 
 def main():
-    response = requests.get(f"{NASA_API_ENDPOINT}?startDate={time_start}&endDate={time_now}&api_key={API_KEY}").json()[0]
+    response = requests.get(
+        f"{NASA_API_ENDPOINT}?startDate={time_start}&endDate={time_now}&api_key={API_KEY}"
+    ).json()[0]
     link = response["link"]
     kpIndex = response["allKpIndex"][0]["kpIndex"]
 
@@ -31,5 +20,6 @@ def main():
 
     print(f"Link for the last Geomagnetic Storm event: {link}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
