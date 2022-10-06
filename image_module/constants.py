8bd6eaaeda92803b01b4b3401b7f5b78b1585668
Michael Cuipa <mcuipa@wayfair.com>
2022-10-06 12:37:27 -0400

    Pushing a branch so Zahra can  test  with me

---
+++ image_module/constants.py
@@ -0,0 +1,9 @@
+# NASA API Configuration Values
+NASA_IMAGE_API_KEY="oxO838wRIlOhhWbb8FNY7yh3TAA9Md5aNf7Oq3Pc"
+NASA_APOD_URL= "https://api.nasa.gov/planetary/apod"
+
+# JWSTAPI Configuration Values
+JWSTAPI_API_KEY="ae29c6a7-ffcb-4b49-a0fe-46361039c823"
+JWSTAPI_IMAGE_TYPE_JPG="jpg"
+JWSTAPI_BASE_URL="https://api.jwstapi.com/"
+JWST_SUFFIX="_cal"
---
+++ image_module/images.py
@@ -0,0 +1,49 @@
+import requests
+from typing import List
+
+from image_module.constants import JWSTAPI_API_KEY, \
+    JWSTAPI_IMAGE_TYPE_JPG, \
+    JWSTAPI_BASE_URL, \
+    JWST_SUFFIX, \
+    NASA_APOD_URL, \
+    NASA_IMAGE_API_KEY
+
+jwst_headers = {"X-API-KEY": JWSTAPI_API_KEY}
+
+
+# Get all James Webb Space Telescope images from https://jwstapi.com/
+def jwst_get_all_jpg_images():
+    image_list: List = []
+    try:
+        response = requests.get(f"{JWSTAPI_BASE_URL}/all/type/{JWSTAPI_IMAGE_TYPE_JPG}", headers=jwst_headers).json()
+        body = response['body']
+        for x in body:
+            details = x['details']
+            # Parse out just the full size 2D images and create a list
+            # if details['suffix'] == JWST_SUFFIX:
+            image_list.append({"url": x['location'], "thumbnail": x['thumbnail']})
+        for x in image_list:
+            print(x)
+        return body
+    except Exception as e:
+        print(e)
+
+
+def nasa_astronomy_picture_of_the_day(date: str = None):
+    params = {
+        "api_key": NASA_IMAGE_API_KEY,
+        "date": date
+    } if date else {"api_key": NASA_IMAGE_API_KEY}
+    print(params)
+    try:
+        body = requests.get(NASA_APOD_URL, params=params).json()
+        if body['media_type'] != "image":
+            result = f"No image results available for date: {body['date']}"
+        title = body['title']
+        description = body['explanation']
+        hdurl = body['hdurl']
+        url = body['url']
+        copyright = body['copyright']
+        return
+    except Exception as e:
+        print(e)
