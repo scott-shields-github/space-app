3a88fc7a81fd61c22936584ee649147288303fb7
Chris Bilbo <cbilbo@qnect.com>
2022-10-06 18:32:29 -0500

    bot runner tweak

--- bot_runner.sh
+++ bot_runner.sh
@@ -3,13 +3,29 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
+LOGS_DIR="logs"
+LOG_FILE="bot_logging"
+LOG_LOC=$LOGS_DIR/$LOG_FILE
+echo "Bot runner has started"
+
 echo "Starting up the bot"
+if [ ! -d "$LOGS_DIR" ]; then
+  echo "Creating logs directory"
+  mkdir -p $LOGS_DIR
+fi
+# bot runner is running inside a venv right now, but should really make the venv itself
+
+echo "Pulling from github to get latest version and "
 git pull
-pip3 install -r requirements.txt
-python3 app.py &
-BOT_PID=$!
+
 CURRENT_HASH=$(git rev-parse --short HEAD)
-echo "Current commit hash: $CURRENT_HASH"
+echo "Starting commit hash: $CURRENT_HASH"
+
+exec 3>&1 >> $LOG_LOC-$CURRENT_HASH 2>&1
+pip3 install -r requirements.txt
+python3 -u app.py &
+BOT_PID=$!
+
 
 while true
 do
@@ -20,9 +36,11 @@ do
     echo "Need to update branch to new hash $MAIN_HASH"
     git pull
     CURRENT_HASH=$(git rev-parse --short HEAD)
+    exec 3>&1 >> $LOG_LOC-$CURRENT_HASH 2>&1
+    echo "Start of new commit hash"
     pip3 install -r requirements.txt
     kill $BOT_PID
-    python3 app.py &
+    python3 -u app.py &
     BOT_PID=$!
   else
     sleep 60
